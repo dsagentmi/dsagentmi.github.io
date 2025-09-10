1e23c8085e45f67f4abef1f4921ae8b2ee6d260f
dsagentmi <agent.designspace@web.de>
2025-09-10 12:51:58 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       
+      <head>
+        <meta name="robots" content="noindex"></meta>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
